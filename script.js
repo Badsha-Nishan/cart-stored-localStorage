5fcf641842f559fc14f3f455dba192a10d8071f0
Badsha-Nishan <badshanisan14@gmail.com>
2026-03-10 16:42:43 +0600

    added cart stored in localStorage and presistant card

--- script.js
+++ script.js
@@ -1,28 +1,66 @@
-const product = document.getElementById("product");
-const quantity = document.getElementById("quantity");
-const addBtn = document.getElementById("add-btn");
-const cartContainer = document.getElementById("cartContainer");
+const handleProductCart = () => {
+  const productEl = document.getElementById("product");
+  const quantityEl = document.getElementById("quantity");
+  const product = productEl.value;
+  const quantity = parseInt(quantityEl.value);
+  displayCart(product, quantity);
+  addProductToCart(product, quantity);
+  productEl.value = "";
+  quantityEl.value = "";
+};
 
-addBtn.addEventListener("click", () => {
-  const productValue = product.value;
-  const quantityValue = quantity.value;
-  console.log(productValue, quantityValue);
-  displayCart(productValue, quantityValue);
-  setLS(productValue, quantityValue)
-});
+const getCart = () => {
+  let cart = {};
+
+  const cartJSON = localStorage.getItem("cart");
+  if (cartJSON) {
+    cart = JSON.parse(cartJSON);
+  }
+  return cart;
+};
+
+const addProductToCart = (product, quantity) => {
+  const cart = getCart();
+  if (cart[product]) {
+    cart[product] += quantity;
+  } else {
+    cart[product] = quantity;
+  }
+  const cartJSON = JSON.stringify(cart);
+  localStorage.setItem("cart", cartJSON);
+};
 
 const displayCart = (product, quantity) => {
   const li = document.createElement("li");
   li.innerText = `${product}: ${quantity}`;
+  const cartContainer = document.getElementById("cartContainer");
   cartContainer.appendChild(li);
-  getLS(li)
 };
 
-const setLS = (product, quantity)=>{
-    localStorage.setItem(product, quantity)
-}
+const displayStoredProduct = () => {
+  const cart = getCart();
+  for (const product in cart) {
+    const quantity = cart[product];
+    displayCart(product, quantity);
+  }
+};
 
-const getLS = (getProduct)=>{
-    console.log(getProduct)
-    localStorage.getItem(getProduct)
-}
+displayStoredProduct();
+
+// My own logic not full code just try
+// addBtn.addEventListener("click", () => {
+//   const productValue = product.value;
+//   const quantityValue = quantity.value;
+//   displayCart(productValue, quantityValue);
+//   setLS(productValue, quantityValue);
+// });
+
+// const displayCart = (product, quantity) => {
+//   const li = document.createElement("li");
+//   li.innerText = `${product}: ${quantity}`;
+//   cartContainer.appendChild(li);
+// };
+
+// const setLS = (product, quantity) => {
+//   localStorage.setItem(product, quantity);
+// };
